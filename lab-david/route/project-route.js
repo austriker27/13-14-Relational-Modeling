89394bdf1b750f6a16fbd138119deb5b5da9ee96
David Lindahl <davidalindahl@gmail.com>
2017-12-13 15:22:30 -0800

    refactor routes for new model

--- lab-david/route/project-route.js
+++ lab-david/route/project-route.js
@@ -3,58 +3,58 @@
 const {Router} = require('express');
 const jsonParser = require('body-parser').json();
 
-const Mountain = require('../model/mountain');
+const Project = require('../model/project');
 const logger = require('../lib/logger');
 const httpErrors = require('http-errors');
 
-const mountainRoute = module.exports = new Router();
+const projectRoute = module.exports = new Router();
 
-mountainRoute.post('/api/mountains', jsonParser, (request,response,next) => {
-  if(!request.body.name || !request.body.state || !request.body.range) {
-    return next(httpErrors(400, 'body and content are required' ));
+projectRoute.post('/api/projects', jsonParser, (request,response,next) => {
+  if(!request.body.title || !request.body.language || !request.body.year) {
+    return next(httpErrors(400, 'title, language and year are required things' ));
   }
 
-  return new Mountain(request.body).save()
-    .then(mountain => {
-      return response.json(mountain);
+  return new Project(request.body).save()
+    .then(project => {
+      return response.json(project);
     })
     .catch(next);
 });
 
-mountainRoute.get('/api/mountains/:id', (request,response,next) => {
-  return Mountain.findById(request.params.id)
-    .then(mountain => {
-      if(!mountain){
-        throw httpErrors(404,`note not found`);
+projectRoute.get('/api/projects/:id', (request,response,next) => {
+  return Project.findById(request.params.id)
+    .then(project => {
+      if(!project){
+        throw httpErrors(404,`project not found`);
       }
       logger.log('info', 'GET - returning a 200 status code');
-      return response.json(mountain);
+      return response.json(project);
     }).catch(next);
 });
 
-mountainRoute.delete('/api/mountains/:id',(request,response,next) => {
-  return Mountain.findByIdAndRemove(request.params.id)
-    .then(mountain => {
-      if(!mountain){
-        throw httpErrors(404,`mountain was not found. go back to start, do not collect $200.`);
+projectRoute.delete('/api/projects/:id',(request,response,next) => {
+  return Project.findByIdAndRemove(request.params.id)
+    .then(project => {
+      if(!project){
+        throw httpErrors(404,`project was not found. go back to start, do not collect $200.`);
       }
       logger.log('info', `GET - returning a 204 status code`);
       return response.sendStatus(204);
     }).catch(next);
 });
 
-mountainRoute.get('/api/mountains/', (request,response) => {
+projectRoute.get('/api/projects/', (request,response) => {
   logger.log('info', 'GET - processing for a non-ID specific request');
 
-  Mountain.find({})
-    .then(mountain => {
-      if(!mountain){
+  Project.find({})
+    .then(project => {
+      if(!project){
         logger.log('info', 'GET - returning a 404 status code');
         return response.sendStatus(404);
       }
       logger.log('info', 'GET - returning a 200 status code');
-      logger.log('info',mountain);
-      return response.json(mountain);
+      logger.log('info',project);
+      return response.json(project);
     }).catch(error => {
       if(error.message.indexOf('Cast to ObjectId failed') > -1){
         logger.log('info', 'GET - returning a 404 status code. could not parse the id');
@@ -66,20 +66,20 @@ mountainRoute.get('/api/mountains/', (request,response) => {
     });
 });
 
-mountainRoute.delete('/api/mountains/:id', (request,response) => {
+projectRoute.delete('/api/projects/:id', (request,response) => {
   logger.log('info', 'DELETE - processing a delete request for a specific id');
 
-  Mountain.findById(request.params.id)
-    .then(mountain => {
-      if(!mountain){
+  Project.findById(request.params.id)
+    .then(project => {
+      if(!project){
         logger.log('info', 'DELETE - returning a 404 status code');
         return response.sendStatus(404);
       }
       logger.log('info', 'DELETE - returning a 200 status code');
-      logger.log('info',mountain);
+      logger.log('info',project);
 
-      response.json(mountain).delete();
-      return response.json(mountain);
+      response.json(project).delete();
+      return response.json(project);
 
     }).catch(error => {
       if(error.message.indexOf('Cast to ObjectId failed') > -1){
@@ -92,15 +92,15 @@ mountainRoute.delete('/api/mountains/:id', (request,response) => {
     });
 });
 
-mountainRoute.put('/api/mountains/:id', jsonParser,(request,response,next) => {
+projectRoute.put('/api/projects/:id', jsonParser,(request,response,next) => {
   let options = {runValidators: true, new : true};
 
-  return Mountain.findByIdAndUpdate(request.params.id,request.body,options)
-    .then(mountain => {
-      if(!mountain){
-        throw httpErrors(404, 'mountain was not found');
+  return Project.findByIdAndUpdate(request.params.id,request.body,options)
+    .then(project => {
+      if(!project){
+        throw httpErrors(404, 'project was not found');
       }
       logger.log('info', 'GET - returning a 200 status code');
-      return response.json(mountain);
+      return response.json(project);
     }).catch(next);
 });
